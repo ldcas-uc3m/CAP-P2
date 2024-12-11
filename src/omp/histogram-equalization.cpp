0f7c2ab79c45ca3e053f3dfc85b372679e334e34
Lucas Gallego <78802401+Cocytus72@users.noreply.github.com>
2024-12-11 17:12:43 +0100

    Update histogram-equalization.cpp

--- src/omp/histogram-equalization.cpp
+++ src/omp/histogram-equalization.cpp
@@ -8,40 +8,15 @@
 void histogram(int * hist_out, unsigned char * img_in, int img_size, int nbr_bin){
     int i, threads_number;
 
-    /*Esta correcto*/
-    omp_set_dynamic(true);
-    #pragma omp parallel for simd
+    #pragma omp parallel for schedule(static)
     for ( i = 0; i < nbr_bin; i ++){
         hist_out[i] = 0;
     }
 
-    /*Funciona*/
-    omp_set_dynamic(true);
-    #pragma omp parallel
-    {
-        int thread_id = omp_get_thread_num();
-        threads_number = omp_get_num_threads();
-
-        // Creamos un histograma privado para cada hilo
-        int private_thread_hist[nbr_bin] = {0};
-
-        //Calcular el histgrama de manera local
-        #pragma omp for
-        for ( i = 0; i < img_size; i ++){
-            private_thread_hist[img_in[i]]++;
-        }
-
-        #pragma omp critical
-        {
-            for ( i = 0; i < nbr_bin; i ++){
-                hist_out[i] += private_thread_hist[i];
-            }
-        }
-    }
-    
-    /*for ( i = 0; i < img_size; i ++){
+    #pragma omp parallel for reduction(+: hist_out[:nbr_bin])
+    for ( i = 0; i < img_size; i ++){
         hist_out[img_in[i]] ++;
-    }*/
+    }
 }
 
 void histogram_equalization(unsigned char * img_out, unsigned char * img_in,
@@ -53,22 +28,26 @@ void histogram_equalization(unsigned char * img_out, unsigned char * img_in,
     min = 0;
     i = 0;
     
-    /*#pragma omp parallel
-    while(min == 0){
-        min = hist_in[i++];
-    }*/
+    #pragma omp parallel
+    {
+        int min_local = 0;
 
-    #pragma omp parallel for
-    for(i = 0; i < nbr_bin; i++){
-        if (min == 0 && hist_in[i] != 0 ){
-            #pragma omp critical
-            min = hist_in[i];
+        #pragma omp parallel for simd
+        for(i = 0; i < nbr_bin; i++){
+            if (min_local == 0 && hist_in[i] != 0){
+                min_local = hist_in[i];
+            }
+        }
+        
+        #pragma omp critical
+        if (min == 0 || (min_local != 0 && min_local > min)){
+            min = min_local;
         }
     }
+
+
     d = img_size - min;
 
-    
-    /*Sigo trabajando en este*/
     for(i = 0; i < nbr_bin; i ++){
         cdf += hist_in[i];
         //lut[i] = (cdf - min)*(nbr_bin - 1)/d;
@@ -80,7 +59,6 @@ void histogram_equalization(unsigned char * img_out, unsigned char * img_in,
     
 
     /* Get the result image */
-    omp_set_dynamic(true);
     #pragma omp parallel for schedule(static)
     for(i = 0; i < img_size; i ++){
         if(lut[img_in[i]] > 255){
