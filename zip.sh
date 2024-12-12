5b00719c12ae7c24e64601a3291088ebf90a8c14
Luis Daniel Casais Mezquida <luisdaniel.casais@alumnos.uc3m.es>
2024-12-12 23:34:31 +0100

    Update zip.sh

--- zip.sh
+++ zip.sh
@@ -17,7 +17,7 @@ cp report/report.pdf .
 
 # zip it (excluding useless stuff)
 echo "Zipping..."
-zip -r $OUTFILE . -x zip.sh report/\* \*.git\* img/\* *__pycache__/\* .venv/\* build/\* .vscode/\* LICENSE
+zip -r $OUTFILE . -x zip.sh report/\* \*.git\* img/\* data/\* *__pycache__/\* .venv/\* build/\* .vscode/\* LICENSE
 
 # cleanup
 echo "Cleaning up..."
